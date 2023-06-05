bca1d67108f78ea4400a9ec6cfa1cd7d41b082e5
Agustin Kassis <webmaster@masize.com>
2023-06-05 18:58:18 -0300

    aesthetic changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,11 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import CaravanAnimation from "~/components/CaravanAnimation";
-import Logo from "~/components/Logo";
 import textLogoSrc from "../../public/images/logo-text.svg";
 import bannerSrc from "../../public/images/banner.jpg";
 import Image, { type StaticImageData } from "next/image";
 import Countdown from "~/components/Countdown";
+import Footer from "~/components/Footer";
 
 const Home: NextPage = () => {
   return (
@@ -16,14 +16,14 @@ const Home: NextPage = () => {
         <meta name="description" content="Desarrollado por La Crypta" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-white bg-bitcoin text-center text-black">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-black bg-bitcoin text-center text-white">
+        <div className="container flex flex-col items-center justify-center px-4 py-16 ">
           <div className="flex flex-col items-center  font-extrabold tracking-tight">
             <h2 className="text-3xl sm:text-[3rem]">Sumate a</h2>
             <h1 className="text-5xl sm:text-[5rem]">La Caravana de Bitcoin</h1>
           </div>
 
-          <div className="flex flex-col items-center justify-center space-y-6">
+          <div className="flex flex-col items-center justify-center space-y-11">
             <div className="text-4xl">
               Este <b>Sábado 10 de Junio, 14hs</b>
             </div>
@@ -55,6 +55,20 @@ const Home: NextPage = () => {
               </div>
             </div>
 
+            <div className="w-full text-left text-2xl">
+              <div className="text-4xl">Regalamos</div>
+              <div>- 50 paraguas naranjas</div>
+              <div>- 100 remeras custom para el evento</div>
+              <div>- sorpresas</div>
+            </div>
+
+            <div className="w-full text-left text-2xl">
+              <div className="text-4xl">Vamos caminando en caravana</div>
+              <div>- 20 minutos</div>
+              <div>- Música al palo</div>
+              <div>- Reclutando gente</div>
+            </div>
+
             <div className="text-2xl">
               ⬇️⬇️ Registrate GRATIS en EventBrite ⬇️⬇️
             </div>
@@ -73,12 +87,7 @@ const Home: NextPage = () => {
             </div>
           </div>
         </div>
-        <div className="flex w-full flex-col items-center justify-center bg-gray-200 p-9">
-          <div>Organizado por </div>
-          <Link href="https://links.lacrypta.ar" target="_blank">
-            <Logo width={200} height={200} />
-          </Link>
-        </div>
+        <Footer />
       </main>
     </>
   );
