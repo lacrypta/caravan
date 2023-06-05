9a106bb9f5c9c9f4297390fdc29bb1cdb108909e
Agustin Kassis <webmaster@masize.com>
2023-06-05 19:02:56 -0300

    fixed Countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react";
 import ReactCountdown from "react-countdown";
 
 export const Countdown = () => {
-  const [date, setDate] = useState<number>(0);
+  const [date, setDate] = useState<number>();
   useEffect(() => {
     setDate(new Date("2023-06-10T17:00:00.000Z").getTime());
   }, []);
-  return <ReactCountdown className="text-6xl" date={date} />;
+  return date ? <ReactCountdown className="text-6xl" date={date} /> : <></>;
 };
 
 export default Countdown;
