3bb89dc1a371d3fd4766e1d22ae33b4e52d87674
Agustin Kassis <webmaster@masize.com>
2023-06-05 18:58:01 -0300

    fixed countdownbug

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from "react";
 import ReactCountdown from "react-countdown";
 
-const date = new Date("2023-06-10T17:00:00.000Z").getTime();
-
 export const Countdown = () => {
+  const [date, setDate] = useState<number>(0);
+  useEffect(() => {
+    setDate(new Date("2023-06-10T17:00:00.000Z").getTime());
+  }, []);
   return <ReactCountdown className="text-6xl" date={date} />;
 };
 
