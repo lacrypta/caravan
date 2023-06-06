5e02c29a074d8430547a82a940b431ec6f8a5e98
Agustin Kassis <webmaster@masize.com>
2023-06-05 23:44:11 -0300

    added google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 import { type AppType } from "next/dist/shared/lib/utils";
 import "~/styles/globals.css";
 import "~/styles/fonts.css";
 
+const tagManagerArgs = {
+  gtmId: "G-ZT5QKKRCES",
+};
+
 const MyApp: AppType = ({ Component, pageProps }) => {
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+
   return <Component {...pageProps} />;
 };
 
