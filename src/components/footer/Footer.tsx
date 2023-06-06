e3fcacda95e1ba7283f32baa26088fe3cef4f38e
Agustin Kassis <webmaster@masize.com>
2023-06-06 16:15:31 -0300

    updated footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,7 @@ import Logo from "./Logo";
 
 export const Footer = () => {
   return (
-    <div className="flex w-full flex-col items-center justify-center bg-gray-900 p-9">
-      <div>Organizado por </div>
+    <div className="flex w-full flex-col items-center justify-center bg-gray-950 p-9">
       <Link href="https://links.lacrypta.ar" target="_blank">
         <Logo width={200} height={200} />
       </Link>
