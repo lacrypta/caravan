b5f5ccfc63b48f9fe565f4da6aef575eb14056bc
Agustin Kassis <webmaster@masize.com>
2023-06-05 21:59:50 -0300

    show loader

--- src/components/CaravanAnimation.tsx
+++ src/components/CaravanAnimation.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image";
 import caravanSrc from "../../public/images/caravan.gif";
+import { useEffect, useState } from "react";
 
 export const CaravanAnimation = () => {
-  return <Image src={caravanSrc} alt="Mapa Caravana" />;
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <div>
+      <Image
+        src={caravanSrc}
+        loading="eager"
+        alt="Mapa Caravana"
+        onLoad={() => setLoaded(true)}
+      />
+      {!loaded && <div>Generando mapa...</div>}
+    </div>
+  );
 };
 
 export default CaravanAnimation;
