35efbbe0ff59e09df3d4155ce14adf67afe49dcd
Agustin Kassis <webmaster@masize.com>
2023-06-06 16:22:22 -0300

    updated header meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,12 +14,40 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Caravana Lightning - La Crypta</title>
-        <meta name="description" content="Desarrollado por La Crypta" />
+        <title>La Caravana Bitcoiner - La Crypta</title>
+        <meta
+          name="description"
+          content="La Caravana Bitcoiner desde La Crypta para la #BGigante"
+        />
+
+        {/* Verification tags */}
         <meta
           name="facebook-domain-verification"
           content="rqzhaxm4c76o8eja0seova4eqlq9ki"
         />
+
+        {/* Facebook */}
+        <meta property="og:title" content="La Caravana Bitcoiner" />
+        <meta property="og:type" content="event.lacrypta" />
+        <meta property="og:url" content="https://caravana.lacrypta.ar/" />
+        <meta
+          property="og:image"
+          content="https://caravana.lacrypta.ar/images/banner.png"
+        />
+
+        {/* Twitter metatags */}
+        <meta name="twitter:title" content="La Caravana Bitcoiner" />
+        <meta
+          name="twitter:description"
+          content="La Caravana Bitcoiner desde La Crypta para la #BGigante"
+        />
+        <meta
+          name="twitter:image"
+          content="https://caravana.lacrypta.ar/images/banner.png"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        {/* Favicon */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-black bg-bitcoin text-center text-white">
