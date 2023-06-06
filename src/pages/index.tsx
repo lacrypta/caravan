11190d7256c5da17ce8f7558eb2d162a79bf94b6
Agustin Kassis <webmaster@masize.com>
2023-06-05 21:28:48 -0300

    text fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -61,12 +61,13 @@ const Home: NextPage = () => {
               <h1 className="text-4xl">Regalamos</h1>
               <div>- 50 paraguas naranjas</div>
               <div>- 100 remeras custom para el evento</div>
-              <div>- sorpresas</div>
+              <div>- 1000 stickers piolas</div>
+              <div>- más sorpresas...</div>
             </div>
 
             <div className="w-full text-left text-2xl">
-              <h1 className="text-4xl">Vamos caminando en caravana</h1>
-              <div>- 20 minutos</div>
+              <h1 className="text-4xl">Vamos en caravana</h1>
+              <div>- 20 minutos de caminata</div>
               <div>- Música al palo</div>
               <div>- Reclutando gente</div>
             </div>
@@ -79,7 +80,7 @@ const Home: NextPage = () => {
             </div>
 
             <div className="text-2xl">
-              <h1>⬇️ Registrate GRATIS en EventBrite ⬇️</h1>
+              <h1>⬇️ Reservá GRATIS en EventBrite ⬇️</h1>
             </div>
             <div>
               <Link
