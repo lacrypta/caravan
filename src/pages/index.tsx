370c81590aea0d9bbcdd65870f9e330edf3bb5ca
Agustin Kassis <webmaster@masize.com>
2023-06-05 21:26:19 -0300

    added bitcoin color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,9 @@ const Home: NextPage = () => {
         <div className="container flex flex-col items-center justify-center px-4 py-16 ">
           <div className="flex flex-col items-center  font-extrabold tracking-tight">
             <h2 className="text-3xl sm:text-[3rem]">Sumate a</h2>
-            <h1 className="text-5xl sm:text-[5rem]">La Caravana de Bitcoin</h1>
+            <h1 className="text-5xl sm:text-[5rem]">
+              La Caravana de <span className="text-orange-400">BITCOIN</span>
+            </h1>
           </div>
 
           <div className="flex flex-col items-center justify-center space-y-11">
@@ -91,6 +93,9 @@ const Home: NextPage = () => {
                   height={400}
                 />
               </Link>
+            </div>
+            <div className="text-2xl">
+              <h1>⬆️ Hacé click el banner ⬆️</h1>
             </div>
           </div>
         </div>
