cf32a7954b42e9a8ebe113e62306b8ea3ecdc632
Agustin Kassis <webmaster@masize.com>
2023-06-05 21:15:49 -0300

    added fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,10 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        blatant: ["Blatant", "Arial"],
+        standerd: ["Standerd", "Trebuchet MS"],
+      },
       backgroundImage: {
         bitcoin: "url('/images/background.png')",
       },
