da052faf3b1fe8b8ae174101724624b3ba0a41a4
Agustin Kassis <webmaster@masize.com>
2023-06-06 17:09:33 -0300

    fixed responsive

--- src/components/header/Title.tsx
+++ src/components/header/Title.tsx
@@ -28,50 +28,53 @@ export const Title = () => {
         sumate a
       </motion.h2>
       <motion.h1
-        className="flex flex-row items-center justify-center space-x-4 text-7xl sm:text-[5rem]"
+        className="flex flex-col items-center  justify-center space-x-6 text-6xl md:flex-row md:text-[4rem] lg:text-[5rem]"
         variants={{
           hidden: { scale: 10, opacity: 0 },
           show: { scale: 1, opacity: 1 },
         }}
         transition={{ duration: 0.4 }}
       >
-        <motion.div
-          className=" text-gray-100"
-          initial={{ scale: 1 }}
-          animate={{
-            scale: [1, 1.04, 1.07, 1, 1],
-            rotate: [0, 1.04, 1, 0, -1.05, 0],
-            x: [0, 7, 10, 0, 0],
-          }}
-          transition={{ repeat: Infinity, duration: 7.2 }}
-        >
-          #
-        </motion.div>
+        <div className="flex flex-row space-x-4">
+          <motion.div
+            className="text-gray-100"
+            initial={{ scale: 1 }}
+            animate={{
+              scale: [1, 1.04, 1.07, 1, 1],
+              rotate: [0, 1.04, 1, 0, -1.05, 0],
+              x: [0, 7, 10, 0, 0],
+            }}
+            transition={{ repeat: Infinity, duration: 7.2 }}
+          >
+            #
+          </motion.div>
 
-        <motion.div
-          className=" text-gray-100"
-          initial={{ scale: 1 }}
-          animate={{
-            scale: [1.03, 1.04, 1.07, 1, 1, 1.03],
-            rotate: [0, 1.04, 0, 0, -1.1, 0],
-            x: [0, 7, 10, 0, 0, 5, 0],
-          }}
-          transition={{ repeat: Infinity, duration: 9 }}
-        >
-          La
-        </motion.div>
-        <motion.div
-          className=" text-gray-100"
-          initial={{ scale: 1 }}
-          animate={{
-            scale: [1, 1.04, 1.07, 1, 1, 1],
-            rotate: [-1.1, 0, 1.1, 0, 0, 0, -1.1],
-            x: [0, 7, 10, 0, 0, 1, 0],
-          }}
-          transition={{ repeat: Infinity, duration: 6 }}
-        >
-          Caravana
-        </motion.div>
+          <motion.div
+            className=" text-gray-100"
+            initial={{ scale: 1 }}
+            animate={{
+              scale: [1.03, 1.04, 1.07, 1, 1, 1.03],
+              rotate: [0, 1.04, 0, 0, -1.1, 0],
+              x: [0, 7, 10, 0, 0, 5, 0],
+            }}
+            transition={{ repeat: Infinity, duration: 9 }}
+          >
+            La
+          </motion.div>
+          <motion.div
+            className=" text-gray-100"
+            initial={{ scale: 1 }}
+            animate={{
+              scale: [1, 1.04, 1.07, 1, 1, 1],
+              rotate: [-1.1, 0, 1.1, 0, 0, 0, -1.1],
+              x: [0, 7, 10, 0, 0, 1, 0],
+            }}
+            transition={{ repeat: Infinity, duration: 6 }}
+          >
+            Caravana
+          </motion.div>
+        </div>
+
         <motion.div
           className=" text-orange-400"
           initial={{ scale: 1 }}
